f5d9370ba0dbd6f162271b5a73b131200bfbb2e2
nxs4246 <nxs4246@mavs.uta.edu>
2024-06-10 01:31:30 -0500

    able to play one round of rock paper scissor

--- rps.js
+++ rps.js
@@ -13,15 +13,60 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let choice = prompt("Choose between rock, paper or scissor:");
+    let choice = prompt("Choose between rock, paper or scissor:").toLowerCase();
     while (choice !== "rock" && choice !== "paper" && choice !== "scissor") {
         choice = prompt("Wrong input! Please try again!\nChoose between rock, paper or scissor:");
     }
     return choice;
 }
 
-let h = getHumanChoice();
-let c = getComputerChoice();
+function playRound(humanChoice, computerChoice) {
+    switch(humanChoice) {
+        case "rock":
+            if(computerChoice == "rock") {
+                console.log("It's a draw!");
+            } else if(computerChoice == "paper") {
+                console.log("You lose! Paper beats Rock.");
+                computerScore++;
+            } else {
+                console.log("You win! Rock beats Scissor.")
+                humanScore++;
+            }
+            break;
+        
+        case "paper":
+            if(computerChoice == "paper") {
+                console.log("It's a draw!");
+            } else if(computerChoice == "scissor") {
+                console.log("You lose! Scissor beats Paper.");
+                computerScore++;
+            } else {
+                console.log("You win! Paper beats Rock.")
+                humanScore++;
+            }
+            break;
 
-console.log(h);
-console.log(c);
+        case "scissor":
+            if(computerChoice == "scissor") {
+                console.log("It's a draw!");
+            } else if(computerChoice == "rock") {
+                console.log("You lose! Rock beats Scissor.");
+                computerScore++;
+            } else {
+                console.log("You win! Scissor beats Paper.")
+                humanScore++;
+            }
+            break;
+    }
+}
+
+let humanScore = 0; 
+let computerScore = 0;
+
+const h = getHumanChoice();
+const c = getComputerChoice();
+
+playRound(h, c);
+
+console.log(humanScore);
+console.log(computerScore);
