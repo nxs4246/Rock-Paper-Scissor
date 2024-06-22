2d28b92fb208adbe84131fdec88b2c96d4c0a4c8
nxs4246 <nxs4246@mavs.uta.edu>
2024-06-22 01:10:48 -0500

    added 3 buttons

--- rps.js
+++ rps.js
@@ -20,55 +20,87 @@ function getHumanChoice() {
     return choice;
 }
 
+/* function getHumanChoice() {
+    let choice = "";
+
+    const btn1 = document.querySelector("#rock");
+    btn1.addEventListener("click", () => {
+        choice = "rock";
+        console.log(choice);
+    })
+
+    const btn2 = document.querySelector("#paper");
+    btn2.addEventListener("click", () => {
+        choice = "paper";
+        console.log(choice);
+    })
+
+    const btn3 = document.querySelector("#scissor");
+    btn3.addEventListener("click", () => {
+        choice = "scissor";
+        console.log(choice);
+    })
+
+    return choice;
+} */
+
+function playRound(humanChoice, computerChoice) {
+
+    let humanScore = 0; 
+    let computerScore = 0;
+
+    switch(humanChoice) {
+        case "rock":
+            if(computerChoice == "rock") {
+                console.log("It's a draw!");
+            } else if(computerChoice == "paper") {
+                console.log("You lose! Paper beats Rock.");
+                computerScore++;
+            } else {
+                console.log("You win! Rock beats Scissor.")
+                humanScore++;
+            }
+            break;
+        
+        case "paper":
+            if(computerChoice == "paper") {
+                console.log("It's a draw!");
+            } else if(computerChoice == "scissor") {
+                console.log("You lose! Scissor beats Paper.");
+                computerScore++;
+            } else {
+                console.log("You win! Paper beats Rock.")
+                humanScore++;
+            }
+            break;
+
+        case "scissor":
+            if(computerChoice == "scissor") {
+                console.log("It's a draw!");
+            } else if(computerChoice == "rock") {
+                console.log("You lose! Rock beats Scissor.");
+                computerScore++;
+            } else {
+                console.log("You win! Scissor beats Paper.")
+                humanScore++;
+            }
+            break;
+    }
+
+    return {humanScore, computerScore};
+}
+
 function playGame() {
 
     let humanScore = 0; 
     let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        switch(humanChoice) {
-            case "rock":
-                if(computerChoice == "rock") {
-                    console.log("It's a draw!");
-                } else if(computerChoice == "paper") {
-                    console.log("You lose! Paper beats Rock.");
-                    computerScore++;
-                } else {
-                    console.log("You win! Rock beats Scissor.")
-                    humanScore++;
-                }
-                break;
-            
-            case "paper":
-                if(computerChoice == "paper") {
-                    console.log("It's a draw!");
-                } else if(computerChoice == "scissor") {
-                    console.log("You lose! Scissor beats Paper.");
-                    computerScore++;
-                } else {
-                    console.log("You win! Paper beats Rock.")
-                    humanScore++;
-                }
-                break;
-    
-            case "scissor":
-                if(computerChoice == "scissor") {
-                    console.log("It's a draw!");
-                } else if(computerChoice == "rock") {
-                    console.log("You lose! Rock beats Scissor.");
-                    computerScore++;
-                } else {
-                    console.log("You win! Scissor beats Paper.")
-                    humanScore++;
-                }
-                break;
-        }
-    }
-
-    for(let i = 1; i <= 5; i++) {
+    for(let i = 1; i <= 1; i++) {
         let humanChoice = getHumanChoice();
         let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
+        let scores = playRound(humanChoice, computerChoice);
+        humanScore += scores.humanScore;
+        computerScore += scores.computerScore;
 
         console.log("Scores after round " + i + ":");
         console.log("Player Score: " + humanScore);
