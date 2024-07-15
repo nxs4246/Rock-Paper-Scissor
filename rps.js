2d28879f316a5f6b5ea481d829ee377e69c74ed0
nxs4246 <nxs4246@mavs.uta.edu>
2024-07-14 23:32:33 -0500

    added the 5 round mechanism

--- rps.js
+++ rps.js
@@ -1,119 +1,112 @@
+// Global variables using snake_case
+let human_score = 0;
+let computer_score = 0;
+let rounds_played = 0;
+const total_rounds = 5;
 
-function playGame() {
-    let human_score = 0;
-    let computer_score = 0;
+// Human input is read through the click of the buttons and passed onto playGame()
+document.getElementById('rock').addEventListener('click', () => playGame("rock"));
+document.getElementById('paper').addEventListener('click', () => playGame("paper"));
+document.getElementById('scissor').addEventListener('click', () => playGame("scissor"));
 
-    for(let i = 1; i <= 5; i++) {
-        document.getElementById('rock').addEventListener('click', function() {
-            let scores;
-            scores = playRound("rock", getComputerChoice());
-            human_score = human_score + scores.humanScore;
-            computer_score = computer_score + scores.computerScore;
-        });
+// Taking human input and playing the game for 5 rounds
+function playGame(humanChoice) {
+    if (rounds_played >= total_rounds) {
+        return; 
+    }
 
-        document.getElementById('paper').addEventListener('click', function() {
-            let scores;
-            scores = playRound("paper", getComputerChoice());
-            human_score = human_score + scores.humanScore;
-            computer_score = computer_score + scores.computerScore;
-        });
+    let scores = playRound(humanChoice, getComputerChoice());
+    human_score += scores.humanScore;
+    computer_score += scores.computerScore;
+    rounds_played++;
 
-        document.getElementById('scissor').addEventListener('click', function() {
-            let scores;
-            scores = playRound("scissor", getComputerChoice());
-            human_score = human_score + scores.humanScore;
-            computer_score = computer_score + scores.computerScore;
-        });
+    updateScore(human_score, computer_score);
+
+    if (rounds_played >= total_rounds) {
+        declareWinner();
     }
 }
 
+// Function to update scoreboard
 function updateScore(human_score, computer_score) {
     const scoreElem = document.querySelector("#scoreboard");
-
+    scoreElem.textContent = `Scoreboard: Human: ${human_score} - Computer: ${computer_score}`;
 }
 
+// Function to declare winner when 5 are played
+function declareWinner() {
+    const resultElem = document.querySelector("#winner");
+    if (human_score > computer_score) {
+        resultElem.textContent += " You are the overall winner!";
+    } else if (human_score < computer_score) {
+        resultElem.textContent += " The computer is the overall winner!";
+    } else {
+        resultElem.textContent += " It's an overall draw!";
+    }
+}
 
+// Give a random choice between r/p/s
 function getComputerChoice() {
-    let max = 3;
-    let min = 0;
-    let rand = Math.random() * (max - min) + min;
-
-    if(rand >= 0 && rand < 1) {
+    let rand = Math.random() * 3;
+    if (rand < 1) {
         return "rock";
-    } else if(rand >= 1 && rand < 2) {
+    } else if (rand < 2) {
         return "paper";
     } else {
         return "scissor";
     }
 }
 
-function updateResult(message) {
-    const resultElem = document.querySelector("#result");
-    resultElem.textContent = message;
-}
-
-
+// Plays one round of r/p/s
 function playRound(humanChoice, computerChoice) {
-
-    let humanScore = 0; 
+    let humanScore = 0;
     let computerScore = 0;
     let msg = "";
 
-    switch(humanChoice) {
+    switch (humanChoice) {
         case "rock":
-            if(computerChoice == "rock") {
-                console.log("It's a draw!");
-                msg = "It's a draw!";
-                computerScore++;
-                humanScore++;
-            } else if(computerChoice == "paper") {
-                console.log("You lose! Paper beats Rock.");
-                msg = "You lose! Paper beats Rock.";
+            if (computerChoice == "rock") {
+                msg = "This round ends in a draw!";
+            } else if (computerChoice == "paper") {
+                msg = "You lose this round! Paper beats Rock.";
                 computerScore++;
             } else {
-                console.log("You win! Rock beats Scissor.");
-                msg = "You win! Rock beats Scissor.";
+                msg = "You win this round! Rock beats Scissor.";
                 humanScore++;
             }
             break;
-        
+
         case "paper":
-            if(computerChoice == "paper") {
-                console.log("It's a draw!");
-                msg = "It's a draw!";
-                computerScore++;
-                humanScore++;
-            } else if(computerChoice == "scissor") {
-                console.log("You lose! Scissor beats Paper.");
-                msg = "You lose! Scissor beats Paper.";
+            if (computerChoice == "paper") {
+                msg = "This round ends in a draw!";
+            } else if (computerChoice == "scissor") {
+                msg = "You lose this round! Scissor beats Paper.";
                 computerScore++;
             } else {
-                console.log("You win! Paper beats Rock.");
-                msg = "You win! Paper beats Rock.";
+                msg = "You win this round! Paper beats Rock.";
                 humanScore++;
             }
             break;
 
         case "scissor":
-            if(computerChoice == "scissor") {
-                console.log("It's a draw!");
-                msg = "It's a draw!";
-                computerScore++;
-                humanScore++;
-            } else if(computerChoice == "rock") {
-                console.log("You lose! Rock beats Scissor.");
-                msg = "You lose! Rock beats Scissor.";
+            if (computerChoice == "scissor") {
+                msg = "This round ends in a draw!";
+            } else if (computerChoice == "rock") {
+                msg = "You lose this round! Rock beats Scissor.";
                 computerScore++;
             } else {
-                console.log("You win! Scissor beats Paper.");
-                msg = "You win! Scissor beats Paper.";
+                msg = "You win this round! Scissor beats Paper.";
                 humanScore++;
             }
             break;
     }
 
     updateResult(msg);
-    return {humanScore, computerScore};
+    return { humanScore, computerScore };
 }
 
-playGame();
+// Updates result of each round by displaying a message
+function updateResult(message) {
+    const resultElem = document.querySelector("#result");
+    resultElem.textContent = message;
+}
